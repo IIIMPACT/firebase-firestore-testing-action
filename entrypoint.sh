e3a904eeae0a1c2d3c1d0866be10399d9395f12e
Bhekani Khumalo <bhekanik@gmail.com>
2020-01-24 09:34:36 +0200

    Edit Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,11 @@
 # Exit if any subcommand fails
 set -e
 
+if [ -z "$FIREBASE_TOKEN" ]; then
+    echo "FIREBASE_TOKEN is required to run commands with the firebase cli"
+    exit 126
+fi
+
 # Setup node modules if needed
 if [ -e node_modules/.bin/jest ]; then
     setup=""
@@ -11,9 +16,10 @@ else
     if [ -f yarn.lock ]; then
         setup="yarn --non-interactive --silent --ignore-scripts --production=false &&"
     else
-        setup="npm --prefix functions ci -f &&"
+        setup="npm --add-python-to-path='true' --prefix functions ci -f &&"
     fi
 fi
 
+sh -c "firebase $*"
 echo "## Running Jest"
 sh -c "$setup npm run --prefix functions test:ci"
